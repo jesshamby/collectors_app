486407c94e88211255badfa4332870c64b9cbc85
jesshamby <jesshamby99@gmail.com>
2021-09-27 15:10:26 +0100

    Moved functions into a separate php file

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+    /**
+    * Creates a connection to the collectorapp database
+    */
+    function createDBConnection()
+    {
+        $db = new PDO('mysql:host=db; dbname=collectorapp', 'root', 'password');
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+
+    /**
+    * A query to the database that fetches the data in all the fields and creates a recipes assoc array
+    * @return array
+    */
+    function fetchAllRecipes(): array {
+        $query = $db->prepare('SELECT `recipe`, `cuisine`, `time`, `link` FROM `recipes`;');
+        $query->execute();
+        $recipes = $query->fetchAll();
+        return $recipes;
+    }
+
+    /**
+    * Creates recipe cards which have a name as well as stats on cuisine, time and a link to the recipe
+    */
+    function createRecipeCards()
+    {
+        foreach ($recipes as $recipe) {
+            echo '<section class= "recipe_card">';
+            echo '<h2>' . $recipe['recipe'] . '<h2>';
+            echo '<h3>Cuisine: ' . $recipe['cuisine'] . '</h3>';
+            echo '<h3>Time (mins): ' . $recipe['time'] . '</h3>';
+            echo '<a href=' . $recipe['link'] . '>Link to recipe</a>';
+            echo '</section>';
+        }
+    }
