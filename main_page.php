491cf210bc9f6dfb1c95857af4b8ba7b52dd376b
jesshamby <jesshamby99@gmail.com>
2021-09-27 13:51:08 +0100

    Connected to the database and added info for each item onto the webpage.

--- main_page.php
+++ main_page.php
@@ -1,19 +1,29 @@
 <?php
+$db = new PDO('mysql:host=db; dbname=collectorapp', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+//Select all the field in my recipes table and fetch-all to create an assoc array
+$query = $db->prepare('SELECT * FROM `recipes`;');
+$query->execute();
+$recipes = $query->fetchAll();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>My Recipes</title>
 </head>
 <body>
-    <section class="recipe_card">
-        <h2>Name:</h2>
-        <h3>Cuisine:</h3>
-        <h3>Time (mins):</h3>
-        <h3>Link to recipe:</h3>
+    <section class= 'recipe_card'>
+        <?php
+            foreach ($recipes as $recipe) {
+                echo '<h2>' . $recipe["recipe"] . '<h2>';
+                echo '<h3>Cuisine:' . $recipe["cuisine"] . '</h3>';
+                echo '<h3>Time (mins):' . $recipe["time"] . '</h3>';
+                echo '<a href=' . $recipe["link"] .'>Link to recipe</a>';
+            }
+        ?>
     </section>
 </body>
 </html>
