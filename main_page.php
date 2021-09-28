dd8190d3c5e4796833f4a68ebae1eb39ba312878
jesshamby <jesshamby99@gmail.com>
2021-09-28 14:23:04 +0100

    Added an if statment that when a form is submitted checks if the recipe and cuisine fields are submitted and displays an error message if not.

--- main_page.php
+++ main_page.php
@@ -2,6 +2,22 @@
     require_once('functions.php');
     $db = createDBConnection();
     $recipes = fetchAllRecipes($db);
+
+    $recipeErr = $cuisineErr = $timeErr = $linkErr = '';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (empty($_POST['recipe'])) {
+            $recipeErr = "Recipe name is required";
+        } else {
+            $recipe = validateString($_POST['recipe']);
+        }
+
+        if (empty($_POST['cuisine'])) {
+            $cuisineErr = "Cuisine is required";
+        } else {
+            $cuisine = validateString($_POST['cuisine']);
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,11 +30,13 @@
     <?php
         echo createRecipeCards($recipes);
     ?>
-    <form method="post">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
         <label for="recipe">Recipe:</label>
         <input name="recipe" type="text">
+        <span class="error"><?php echo $recipeErr ?></span>
         <label for="cuisine">Cuisine:</label>
         <input name="cuisine" type="text">
+        <span class="error"><?php echo $cuisineErr ?></span>
         <label for="time">Time (mins):</label>
         <input name="time" type="number">
         <label for="link">Link to recipe:</label>
