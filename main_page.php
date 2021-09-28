c5b837222d4429f239feb48b6f1951292b6dc1f0
jesshamby <jesshamby99@gmail.com>
2021-09-28 13:09:53 +0100

    added html form and inputs for adding a new item to the collection

--- main_page.php
+++ main_page.php
@@ -14,6 +14,17 @@
     <?php
         echo createRecipeCards($recipes);
     ?>
+    <form method="post">
+        <label for="recipe">Recipe:</label>
+        <input name="recipe" type="text">
+        <label for="cuisine">Cuisine:</label>
+        <input name="cuisine" type="text">
+        <label for="time">Time (mins):</label>
+        <input name="time" type="number">
+        <label for="link">Link to recipe:</label>
+        <input name="link" type="url">
+        <input type="submit">
+    </form>
 </body>
 </html>
 
