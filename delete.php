cdb1fd9ff509386a71c9c0deb03611900ce06a8c
jesshamby <jesshamby99@gmail.com>
2021-09-30 15:01:52 +0100

    removed action from form

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
     if (isset($_POST['noDelete'], $_POST['yesDelete'])) {
-        header("Locations: main_page.php");
+        header("Location: main_page.php");
         exit;
     }
 ?>
@@ -13,7 +13,7 @@
 </head>
 <body>
     <p>Are you sure you want to permanently delete the recipe: <?php echo $_POST['delete_recipe'] ?></p>
-    <form action="main_page.php" method="post">
+    <form method="post">
         <button type="submit" name="yesDelete" value= "<?php echo $_POST['delete_recipe']?>">Yes</button>
         <button type="submit" name="noDelete">No</button>
     </form>
